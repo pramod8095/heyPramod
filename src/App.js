adfe707a11c8712c6eefd74a9bc9a777f333765f
pramod8095 <74644178+pramod8095@users.noreply.github.com>
2021-12-14 17:17:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
+    <div>
+      <div class='wave -one'></div>
+      <div class='wave -two'></div>
+      <div class='wave -three'></div>
       <Navbar />
       <Container />
-      
+     </div> 
     </div>
   );
 }
